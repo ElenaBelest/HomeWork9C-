77a0a6f89050ed8ff4e052bb44bad9708905d689
Elena <bellenaelena@gmail.com>
2022-09-20 19:16:35 +0300

    добавлено решение задачи 066

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ void Start()
         {
             case 0: return; break;
             case 64: Numbers(); break;
-            // case 66: MinSumRows(); break;
+            case 66: SumNumbersRec(); break;
             // case 88: ProductOfArrays(); break;
             default: Console.WriteLine("error"); break;
         }
@@ -31,6 +31,11 @@ int SetNumber(string numberName)
     int num = Convert.ToInt32(Console.ReadLine());
     return num;
 }
+
+// Задача 64: Задайте значения M и N. Напишите программу,
+//  которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
 
 void Numbers()
 {
@@ -48,3 +53,32 @@ string NumbersRec(int number, int number2)
     if (number <= number2) return $"{number}" + " " + NumbersRec(number + 1, number2);
     else return String.Empty;
 }
+
+// Задача 66: Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void SumNumbersRec()
+{
+    Console.Write("Enter first number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"The sum of the numbers from {number} to {number2} : " + SumRec(number, number2));
+}
+
+int SumRec(int number, int number2)
+{
+    if (number == 0) return (number2 * (number2 + 1)) / 2;            
+    else if (number2 == 0) return (number * (number + 1)) / 2;       
+    else if (number == number2) return number;                       
+    else if (number < number2) return number2 + SumRec(number, number2 - 1); 
+    else return number2 + SumRec(number, number2 + 1);
+}
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
