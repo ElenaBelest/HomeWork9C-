3710101f40c1a72c16dee5b61fc5d268b88de706
Elena <bellenaelena@gmail.com>
2022-09-20 11:06:06 +0300

    добавлено решение задачи 064

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter number > 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter 1: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+string NumbersRec(int number2, int number)
+{
+    if (number2 <= number) return $"{number2}" + " " + NumbersRec(number2 + 1, number);
+    else return String.Empty;
+}
+Console.WriteLine(NumbersRec(number2,number));
